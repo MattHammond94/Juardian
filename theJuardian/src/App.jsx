ae581e79287bc2149c68248344c835b93c5f4803
Matt Hammond <mjhammond94@gmail.com>
2024-03-19 11:01:30 +0000

    Install axios for multiple fetch req's - resolve issue with alt API - get stable

--- theJuardian/src/App.jsx
+++ theJuardian/src/App.jsx
@@ -6,10 +6,10 @@ import Footer from "./components/Footer";
 function App() {
   return (
     <>
-      {/* <Header /> */}
+      <Header />
       <NewsFeed />
-      {/* <Form />
-      <Footer /> */}
+      <Form />
+      <Footer />
     </>
   )
 }
--- theJuardian/src/components/NewsFeed.jsx
+++ theJuardian/src/components/NewsFeed.jsx
@@ -1,24 +1,34 @@
 import { useEffect, useState } from "react";
+import axios from "axios";
 
 const NewsFeed = () => {
   const [articles, setArticles] = useState();
 
   useEffect(() => {
-    fetch(`https://content.guardianapis.com/search?q=&query-fields=headline&show-fields=thumbnail,headline,byline&order-by=newest&api-key=${import.meta.env.VITE_API_KEY}`)
-    .then(response => response.json())
-    .then(data => setArticles(data.response.results))
+
+    const fetchArticles = async () => {
+      const res = await axios.get(`https://content.guardianapis.com/search?q=&query-fields=headline&page-size=30&show-fields=bodyText,thumbnail,headline,byline&order-by=newest&api-key=${import.meta.env.VITE_API_KEY}`);
+      setArticles(res.data.response.results);
+    }
+
+    fetchArticles();
   }, []);
 
   console.log(articles);
 
   return (
     <main>
-      { articles && articles.map((article, index) => (
-        <div className="articleContainer" key={index}>
-          <img src={article.fields.thumbnail} alt="Thumbnail image depciting article" />
-          <h1>{article.fields.headline}</h1>
-        </div>
-      )) }
+      <div className="feedContainer">
+        { articles && articles.map((article, index) => (
+          <div className="articleContainer" key={index}>
+            <a href={article.webUrl} target='_blank' rel="noreferrer">
+              <img src={article.fields.thumbnail} alt="Thumbnail image depciting article" />
+              <h1>{article.fields.headline}</h1>
+              <p>{article.fields.bodyText}</p>
+            </a>
+          </div>
+        ))}
+      </div>
     </main>
   )
 }
