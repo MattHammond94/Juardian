bb276666fbb24e46b37c150aab5a157373440b96
Matt Hammond <mjhammond94@gmail.com>
2024-03-24 18:03:49 +0000

    Minor change to footer

--- theJuardian/src/components/Footer.jsx
+++ theJuardian/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer>
-      <p>Terms and conditions probably apply here</p>
+      <p>© Terms and conditions probably apply here</p>
       <img src="/logo2.png" alt="Brands logo" />
       <p>2024</p>
     </footer>
